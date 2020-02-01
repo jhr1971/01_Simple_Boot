10fabaa9c12b3587cb0ea0800565a7dd47c543d2
unknown <you@example.com>
2020-02-01 16:39:44 +0100

    jhr1971 test sans quality code-essai1

--- src/test/java/com/formation/ApplicationTests.java
+++ src/test/java/com/formation/ApplicationTests.java
@@ -6,6 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.formation.service.Calculator;
 import com.formation.service.MessageBuilder;
 
+
+// Réservé pour les tests d'intégration : Il ne devrait idealement jamais y avoir de Mock
+// par défaut @SpringBootTest se sert de la classe Application.java qui fournit toute la configuration automatique
+// cf https://www.baeldung.com/spring-boot-testing
 @SpringBootTest
 class ApplicationTests {
 
